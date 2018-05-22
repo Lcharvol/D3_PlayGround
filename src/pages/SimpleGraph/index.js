ed4a3ecb3a7bc7ac4a9d1bc2108602386c6547b2
Lcharvol <charvolin0@gmail.com>
2018-05-22 21:45:10 +0200

    WIP

--- src/pages/SimpleGraph/index.js
+++ src/pages/SimpleGraph/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import * as d3 from 'd3';
+import { map } from 'ramda';
+import { compose, withStateHandlers, lifecycle } from 'recompose';
 
-const SimpleGraph = () => (
-    <div/>
-);
+import { simplePoints } from '../../data/simplePoints';
 
-export default SimpleGraph;
+const getData = () => {
+    let initialData = simplePoints;
+    map(d => {
+        d.date = d3.format(".1f").parse;
+    },initialData);
+    return initialData;
+};
+
+const SimpleGraph = ({ width, height })=> {
+
+    const  margin = {top: 5, right: 50, bottom: 20, left: 50};
+    const w = width - (margin.left + margin.right);
+    const h = height - (margin.top + margin.bottom);
+    const transform = 'translate(' + margin.left + ',' + margin.top + ')';
+    const line = () => d3.select("svg").append("line");
+    return (
+        <div>
+            <svg width={width} height={height}>
+                <g transform={transform}>
+                    <path className="line" d={line(getData())} strokeLinecap="round"/>}
+                </g>
+            </svg>
+        </div>
+    );
+};
+
+
+const enhance = compose(
+    withStateHandlers(
+      {
+        width: 800,
+        height:300,
+      },
+      {
+        handleChangeWidth: () => newWidth => ({ width: newWidth }),
+      },
+    ),
+    lifecycle({
+        componentDidMount() {
+        
+        },
+      }),
+  );
+  
+  export default enhance(SimpleGraph);
